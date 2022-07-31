c54713260462491e2c375669c74130c129f3de5f
Dahdo <dndashimye15@gmail.com>
2022-07-31 16:07:27 +0200

    adding custom contacts via the NewContact activity

--- app/src/main/java/com/dahdotech/contactroom/MainActivity.java
+++ app/src/main/java/com/dahdotech/contactroom/MainActivity.java
@@ -1,21 +1,27 @@
 package com.dahdotech.contactroom;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.dahdotech.contactroom.model.Contact;
 import com.dahdotech.contactroom.model.ContactViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int NEW_CONTACT_ACTIVITY_REQUEST_CODE = 1;
     private ContactViewModel contactViewModel;
     private TextView textView;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +39,23 @@ public class MainActivity extends AppCompatActivity {
             }
             textView.setText(builder.toString());
         });
+        fab = findViewById(R.id.add_conctact_fab);
+        fab.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, NewContact.class);
+            startActivityForResult(intent, NEW_CONTACT_ACTIVITY_REQUEST_CODE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == NEW_CONTACT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
+            assert data != null;
+            String name = data.getStringExtra(NewContact.NAME_REPLY);
+            String occupation = data.getStringExtra(NewContact.OCCUPATION_REPLY);
+
+            Contact contact = new Contact(name, occupation);
+            ContactViewModel.insert(contact);
+        }
     }
 }
